2a9ddf0fbfbb8aca0a7bd753af312c7e2e58f3f7
Daniel Wajsman <danielwajsman@mac.com>
2022-01-12 15:15:18 -0800

    test works

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "Visitor navigates to a product page", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    # 1.times do |n|
+      @category.products.create!(
+        # name:  Faker::Hipster.sentence(3),
+        name:  "Product Name",
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    # end
+
+    # save_and_open_screenshot
+    @product = Product.all
+
+  end
+
+  # scenario "They see all products" do
+  #   # ACT
+  #   visit root_path
+
+  #   # DEBUG
+  #   save_screenshot
+
+  #   # VERIFY
+  #   expect(page).to have_css 'article.product'
+  # end
+
+  scenario "They see product details" do
+    visit root_path
+    click_on "Product Name"
+    find('.product-detail')
+
+    # commented out b/c it's for debugging only
+    save_and_open_screenshot
+
+    expect(page).to have_css '.product-detail', count: 1
+  end
+
+
 end
